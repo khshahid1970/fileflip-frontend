5cd6ada40e1b29edb1b86c38b5313a0b67e3fc4d
khshahid1970 <khawaja_shahid_iqbal@hotmail.com>
2025-04-02 17:29:09 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Welcome to FileFlip!</h1>
+      <p>This is the frontend powered by Vite + React.</p>
+    </div>
+  )
+}
+
+export default App
